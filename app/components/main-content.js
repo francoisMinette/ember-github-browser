48163f26127cf0b56534c7ce6e9014ee7031a9a4
Zouc <fminette@gmail.com>
2017-06-07 18:37:02 +0300

    added filters

--- app/components/main-content.js
+++ app/components/main-content.js
@@ -4,50 +4,85 @@ import config from '../config/environment';
 export default Ember.Component.extend({
 	userNotfound : false,
 	listRepo : [],
+	filteredList : [],
 	isLoadingList : false,
-
-	didInsertElement(){
-	},
+	languagesList : ['JavaScript', 'Ruby', 'D', 'Julia', 'Rust', 'Java', 'Scala', 'Elixir', 'Objective-C', 'C', 'LiveScript', 'Haskell', 'Scheme', 'CoffeeScript', 'Nim', 'Racket', 'Erlang', 'Go', 'Dylan', 'Frege', 'Dart', 'Chapel', 'C#', 'Red', 'C++', 'OCaml', 'Haxe', 'Gosu', 'Crystal', 'TypeScript', 'Factor', 'Perl6', 'Python', 'F#'],
 
 	actions:{
-		submitUserName() {
-			var self = this;
+		submitUserName() {//?q=user:francoisminette+language:javascript
+			var self = this,
+				apiUrl = config.apiUrl + "/search/repositories",
+				selectedLanguages = Ember.$('#select-language').val(),
+				username = Ember.$('#username-input').val();
 
-			this.isLoading = true;
+			this.set('isLoading', true);
 			
-			console.log('in submit username');
-
-			if(!Ember.$('#username-input').val().length){
+			//this is to always look for user if no username has been input
+			if(!username.length){
 				Ember.$('#username-input').val('francoisminette');
+				username = 'francoisminette';
 			}
 
-			Ember.$.get(config.apiUrl + "/users/" + Ember.$('#username-input').val()).then(function(response){
-				self.set('userNotfound', false);
-				self.set('isLoadingList', false);
+			apiUrl += '?q=user:' + username;
+
+			for(var i = 0; i < selectedLanguages.length; i++){
+				apiUrl += '+language:' + selectedLanguages[i].toLowerCase();
+			}
+
+			Ember.$.get(apiUrl).then(function(response){
+				self.setProperties({
+					'userNotfound' : false,
+					'isLoadingList' : false
+				});
 				console.log('in promise success', response);
 				
-				if(!response && !response.repos_url){
+				if(!response){
 					return;
 				}
-
-				Ember.$.get(response.repos_url).then(function(list){
-					console.log('in promise success', list);
-					self.set('listRepo', list);
-				}, function(list) {
+				
+				self.setProperties({
+					'listRepo' : response.items,
+					'filteredList' : response.items
 				});
-
-
 			}, function(response) {
-				self.set('userNotfound', true);
-				self.set('isLoadingList', false);
+				self.setProperties({
+					'userNotfound' : true,
+					'isLoadingList' : false
+				});
+				
 			});
 		},
 
 		selectRepo(value) {
-			var selectedRepo = Ember.$.grep(this.listRepo, function(item){ console.log('supposed to loop', item); return item.id == value; })[0];
+			this.set('activeItem', null);
+			var selectedRepo = Ember.$.grep(this.listRepo, function(item){return item.id == value;})[0];
 			console.log('in selectRepo', value, selectedRepo);
-
 			this.set('activeItem', selectedRepo);
+		},
+
+		changePrivacy(value) {
+			var self = this,
+				newList = Ember.$.grep(this.listRepo, 
+				function(item){
+					switch(parseInt(value, 10)){
+						case 0:
+							return true;
+						break;
+						case 1:
+							return item.private == true;
+						break;
+						case 2:
+							return item.private == false;
+						break;
+					}
+				});
+
+			console.log('in changePrivacy', value, newList);
+			this.set('filteredList', newList);
+
+			if(this.activeItem && !Ember.$.grep(newList, function(item){return item.id == self.activeItem.id}).length){
+				this.set('activeItem', null);
+			}
 		}
 	},
 });
--- app/components/repository-item.js
+++ app/components/repository-item.js
@@ -1,22 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	didInsertElement(){
-		var self = this;
+	didInsertElement(){console.log('in didInsertElement item');
+		this.updateAttr();
+	},
+
+	itemObserver : Ember.observer('item', function(){console.log('in observer item');
+		this.updateAttr();
+	}),
+
+	updateAttr : function(){
+		var self = this,
+			tmpArray = [];
 		
 		Ember.$.get(this.item.branches_url.replace('{/branch}', '')).then(function(data){
 			console.log('in promise success', data);
-			self.set('branchesNbr', data.length);
+			self.set('branchesNbr', data.length || 0);
 		}, function(data) {
 			self.set('branchesNbr', 0);
 		});
 
 		Ember.$.get(this.item.languages_url).then(function(data){
-			console.log('in promise success', Ember.$.map(data, function(el) { return el }));
-			self.set('languages', Ember.$.map(data, function(el) { return el }));
+			for (var property in data) {
+				if (data.hasOwnProperty(property)) {
+					tmpArray.push(property);
+				}
+			}
 
-		}, function(data) {
+			self.set('languages', tmpArray.toString() || 'none');
+			console.log('set languages', self.languages);
 		});
 	},
-
 });
